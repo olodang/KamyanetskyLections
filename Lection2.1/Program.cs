79562b97a0e5cce8ab17149ad3c8dc1d75fd3a8e
Morozova Natasha <olodang@gmail.com>
2023-06-23 13:54:57 +0300

    findArray[i]

---
+++ Lection2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {2,6,45,35,22,76,17,18};
+//array[2] = 777;
+int n = array.Length;
+int find = 2;
+int index = 0;
+
+while(index<n)
+{
+    if(array[index]==find)
+        Console.WriteLine(index);
+    index++;
+
+}
