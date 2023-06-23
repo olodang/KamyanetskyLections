51cb0b0dac14e6fc0ec30a5b35dc39f8e6f96c74
Morozova Natasha <olodang@gmail.com>
2023-06-23 15:30:38 +0300

    MethodsToFindIndex

--- Lection2.1/Program.cs
+++ Lection2.1/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] array = {2,6,45,35,22,76,17,18};
+﻿int [] array = {2,6,45,35,2,76,17,18};
 //array[2] = 777;
 int n = array.Length;
 int find = 2;
@@ -7,7 +7,10 @@ int index = 0;
 while(index<n)
 {
     if(array[index]==find)
+    {
         Console.WriteLine(index);
+        break;
+    }
     index++;
 
 }
---
+++ Lection2.2/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{int length = collection.Length;
+int index = 0;
+while(index<length)
+{
+    collection[index]= new Random().Next(0,10);
+    index++;
+}
+}
+void PrintArray(int[]col)
+{
+int count = col.Length;
+int position = 0;
+while(position<count)
+{
+    Console.WriteLine(col[position]);
+    position++;
+}
+
+}
+
+int IndexOf(int[]collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while(index < count)
+        {
+            if(collection[index]==find)
+            {
+                position = index;
+            }
+
+        index++;
+    }    
+    return position;
+}
+
+int []array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);
